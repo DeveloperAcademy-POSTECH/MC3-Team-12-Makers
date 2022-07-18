8c1927c2d0e69db9877d17c94db8b2768ff82ca7
Youth <99013115+kimscastle@users.noreply.github.com>
2022-07-19 03:40:05 +0900

    [ADD] Schedule Data추가

---
+++ Gajeongtongsin/Gajeongtongsin/Models/Schedule.swift
@@ -0,0 +1,13 @@
+//
+//  Schedule.swift
+//  Gajeongtongsin
+//
+//  Created by uiskim on 2022/07/19.
+//
+
+import Foundation
+struct Schedule {
+    let reservedDate: String          //상담신청날짜
+    let scheduleList: [ScheduleInfo]  //스케줄정보들
+    let content: String               //상담내용
+}
