93e5161703e85822b80323146af88b01036ab5a7
Youth <99013115+kimscastle@users.noreply.github.com>
2022-07-20 14:09:55 +0900

    [CHORE] 데이터구조수정

--- Gajeongtongsin/Gajeongtongsin/Models/Schedule.swift
+++ Gajeongtongsin/Gajeongtongsin/Models/Schedule.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct Schedule {
-    let reservedDate: String          //상담신청날짜
+    let reservedDate: String            //상담신청날짜
     let scheduleList: [ScheduleInfo]  //스케줄정보들
     let content: String               //상담내용
 }
