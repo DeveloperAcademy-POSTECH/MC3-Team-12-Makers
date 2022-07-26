f65a9e7c496b106c934182ab2e2da9eb6e51198c
Miller <eotjd8721@naver.com>
2022-07-26 12:14:36 +0900

    [ADD] firebase 관련 함수들 명명

--- Gajeongtongsin/Gajeongtongsin/Global/Service/FirebaseManager.swift
+++ Gajeongtongsin/Gajeongtongsin/Global/Service/FirebaseManager.swift
@@ -13,7 +13,41 @@ final class FirebaseManager {
     private init() {
         
     }
-
-    
+    /// 학부모 예약 업로드
+    func uploadReservations() {
+        
+    }
+    /// 예약정보들 가져오기
+    func fetchReservations() {
+        
+    }
+    /// 학부모 긴급요청하기
+    func uploadEmergency(){
+        
+    }
+    /// 선생님 긴급요청 가져오기
+    func fetchEmergency(){
+        
+    }
+    /// 학부모 메시지 업로드하기
+    func uploadMessage(){
+        
+    }
+    /// 학부모,선생님 메시지들 가져오기
+    func fetchMessages(){
+        
+    }
+    /// 선생님 확정된 예약 업로드하기
+    func uploadConfirmedReservation(){
+        
+    }
+    /// 알림 업로드하기
+    func uploadNotification(){
+        
+    }
+    /// 알림들 가져오기
+    func fetchNotifications(){
+        
+    }
     
 }
