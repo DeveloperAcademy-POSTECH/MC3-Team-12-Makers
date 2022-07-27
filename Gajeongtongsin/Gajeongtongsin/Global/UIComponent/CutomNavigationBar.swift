67e146d27688386dac402ef95415adc943e9f185
Youth <99013115+kimscastle@users.noreply.github.com>
2022-07-27 12:39:55 +0900

    [CHORE] CustomNavigationBar 공용컴포넌트 구성

--- Gajeongtongsin/Gajeongtongsin/Global/UIComponent/CutomNavigationBar.swift
+++ Gajeongtongsin/Gajeongtongsin/Global/UIComponent/CutomNavigationBar.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 class CutomNavigationBar: UIView {
+    
+    var delegate: CustomNavigationBarDelegate?
+    var title: String = ""
+    var imageName: String = ""
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let rightButtonItem : UIButton = {
+        let button = UIButton()
+        button.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    init(title: String, imageName: String){
+        super.init(frame: .zero)
+        self.title = title
+        self.imageName = imageName
+        render()
+        configUI()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+        rightButtonItem.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func render() {
+        addSubview(titleLabel)
+        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 16).isActive = true
+        
+        addSubview(rightButtonItem)
+        rightButtonItem.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        rightButtonItem.trailingAnchor.constraint(equalTo: self.trailingAnchor,constant: -16).isActive = true
+    }
+    
+    func configUI() {
+        titleLabel.text = title
+        rightButtonItem.setImage(UIImage(systemName: imageName), for: .normal)
+    }
+    @objc func tapButton(){
+        delegate?.tapButton()
+    }
+    
 
 }
