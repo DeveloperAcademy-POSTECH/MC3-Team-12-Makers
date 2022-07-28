906d63a1a1bf26b33ee15c984f79f0d9210cad97
Youth <99013115+kimscastle@users.noreply.github.com>
2022-07-29 00:49:41 +0900

    [CHORE] MockData수정(sentDate: String -> Date), toDate익스텐션추가

--- Gajeongtongsin/Gajeongtongsin/Global/Literal/MockData.swift
+++ Gajeongtongsin/Gajeongtongsin/Global/Literal/MockData.swift
@@ -15,18 +15,18 @@ var parent2 = ParentUser(id: "2", sendingMessages: messageList2, childName: "부
 var parent3 = ParentUser(id: "3", sendingMessages: messageList3, childName: "최히로", schedules: scheduleList3)
 
 var messageList1 = [
-    Message(type: .absence, sentDate: "실시간", expectedDate: "7월21일", content: "김유쓰배아픔", isCompleted: false),
-    Message(type: .earlyLeave, sentDate: "실시간", expectedDate: "7월22일", content: "김유쓰놀이공원", isCompleted: false)
+    Message(type: .absence, sentDate: "2022-03-01".toDate()!, expectedDate: "7월21일", content: "김유쓰배아픔", isCompleted: false),
+    Message(type: .earlyLeave, sentDate: "2022-03-04".toDate()!, expectedDate: "7월22일", content: "김유쓰놀이공원", isCompleted: false),
 ]
 
 var messageList2 = [
-    Message(type: .absence, sentDate: "실시간", expectedDate: "7월23일", content: "부니카제주도", isCompleted: false),
-    Message(type: .earlyLeave, sentDate: "실시간", expectedDate: "7월24일", content: "부니카서울", isCompleted: false)
-    ]
+    Message(type: .absence, sentDate: "2022-03-04".toDate()!, expectedDate: "7월23일", content: "부니카제주도", isCompleted: false),
+    Message(type: .earlyLeave, sentDate: "2022-03-12".toDate()!, expectedDate: "7월24일", content: "부니카서울", isCompleted: false)
+]
 
 var messageList3 = [
-    Message(type: .absence, sentDate: "실시간", expectedDate: "7월25일", content: "최히로소개팅", isCompleted: false),
-    Message(type: .earlyLeave, sentDate: "실시간", expectedDate: "7월26일", content: "최히로레브랑데이트", isCompleted: false)
+    Message(type: .absence, sentDate: "2022-03-01".toDate()!, expectedDate: "7월25일", content: "최히로소개팅", isCompleted: false),
+    Message(type: .earlyLeave, sentDate: "2022-03-07".toDate()!, expectedDate: "7월26일", content: "최히로레브랑데이트", isCompleted: false)
 ]
 
 var scheduleList1 = [
@@ -71,3 +71,4 @@ let noti8 = Notification(id: "2", postId: "22", type: .reservation, childName: "
 let noti9 = Notification(id: "3", postId: "33", type: .message, childName: "최히로", content: "99")
 let noti10 = Notification(id: "3", postId: "33", type: .message, childName: "최히로", content: "1010")
 let noti11 = Notification(id: "3", postId: "33", type: .reservation, childName: "최히로", content: "1111")
+
