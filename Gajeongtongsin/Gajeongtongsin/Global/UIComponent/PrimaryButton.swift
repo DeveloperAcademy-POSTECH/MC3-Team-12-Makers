310c1522d37a10bcb86a07e7c2f74c18be523fb1
monic98 <103012323+monic98@users.noreply.github.com>
2022-07-29 14:40:16 +0900

    [ADD] Primary Button 추가

--- Gajeongtongsin/Gajeongtongsin/Global/UIComponent/PrimaryButton.swift
+++ Gajeongtongsin/Gajeongtongsin/Global/UIComponent/PrimaryButton.swift
@@ -7,20 +7,34 @@
 
 import UIKit
 
-class CustomButton: UIButton {
+class PrimaryButton: UIButton {
     
-    init(buttonTitle: String,textColor : UIColor, backgroundColor: UIColor,textSize: CGFloat) {
-        super.init(frame: .zero)
+    init(buttonTitle: String, buttonState: ButtonState) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 1000, height: 50))
         
         self.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .bold)
-        self.setTitle(buttonTitle, for: .normal)  // 버튼 타이틀
-        self.layer.cornerRadius = 3              // 버튼 cornerRadius
-        self.setTitleColor(.white, for: .normal) //버튼 타이틀 색깔
-        self.backgroundColor = backgroundColor      // 버튼 배경화면 색깔
+        self.setTitle("PrimaryButton", for: .normal)  // 버튼 타이틀
+        self.layer.cornerRadius = 3// 버튼 cornerRadius
+        switch buttonState {
+        case .normal:
+            self.setTitleColor(.white, for: .normal) //버튼 타이틀 색깔
+            self.backgroundColor = .Action      // 버튼 배경화면 색깔
+        case .disabled:
+            self.setTitleColor(.lightText, for: .normal) //버튼 타이틀 색깔
+            self.backgroundColor = .LightLine      // 버튼 배경화면 색깔
+        }
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalToConstant: 358).isActive = true
+        self.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+enum ButtonState {
+    case normal
+    case disabled
     
 }
