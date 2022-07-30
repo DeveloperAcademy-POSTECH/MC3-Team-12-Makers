3461d7835c0e422ea62cd955f67ea16e45cc5738
monic98 <103012323+monic98@users.noreply.github.com>
2022-07-30 14:58:04 +0900

    [CHORE] Secondary Button 주석 추가 및 가독성 향상

--- Gajeongtongsin/Gajeongtongsin/Global/UIComponent/SecondaryButton.swift
+++ Gajeongtongsin/Gajeongtongsin/Global/UIComponent/SecondaryButton.swift
@@ -9,12 +9,10 @@ import UIKit
 
 class SecondaryButton: UIButton {
     
+    // MARK: - Init
     init(buttonTitle: String, buttonState: ButtonState) {
         super.init(frame: CGRect(x: 0, y: 0, width: 1000, height: 50))
         
-        self.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        self.setTitle("SecondaryButton", for: .normal)
-        self.layer.cornerRadius = 10
         switch buttonState {
         case.normal:
             self.setTitleColor(.white, for: .normal)
@@ -23,13 +21,23 @@ class SecondaryButton: UIButton {
             self.setTitleColor(.Confirm, for: .normal)
             self.backgroundColor = .black
         }
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        self.heightAnchor.constraint(equalToConstant: 30).isActive = true
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    // MARK: - Funcs
+    func render() {
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        self.setTitle("SecondaryButton", for: .normal)
+        self.layer.cornerRadius = 10
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        self.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
 }
 
